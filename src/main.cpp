f6df1a83884656a4c14256bbb7df8e1b17dbb949
davicoene <davicoenerosa@gmail.com>
2026-07-26 17:29:59 -0300

    add new tests to turn on

--- src/main.cpp
+++ src/main.cpp
@@ -2,12 +2,13 @@
 #include "Relay.h"
 #include "Sensors.h"
 #include "Washer.h"
+#include "Dryer.h"
 #include "WifiManager.h"
 #include "Config.h" // Assumindo que você tem as credenciais aqui
 
 // Se não tiver o Config.h ainda, substitua por suas credenciais:
-const char* WIFI_SSID = "Sua_Rede_WiFi";
-const char* WIFI_PASS = "Sua_Senha";
+const char* WIFI_SSID = "POCO X7 Pro";
+const char* WIFI_PASS = "12345678";
 
 void setup() {
     Serial.begin(115200);
@@ -15,6 +16,7 @@ void setup() {
     Relay.begin();
     Sensor.begin();
     WasherController.begin();
+    DryerController.begin();
     
     // Inicia o WiFi
     Wifi.begin(WIFI_SSID, WIFI_PASS);
@@ -26,7 +28,22 @@ void loop() {
     Relay.update();
     Sensor.update();
     WasherController.update();
+    DryerController.update();
     
     // Mantém a conexão viva
     Wifi.update();
+
+    if (Serial.available() > 0) {
+        char comando = Serial.read();
+        
+        // Se você digitar '1' e der Enter no Monitor Serial
+        if (comando == '1') {
+            Serial.println("Comando recebido: Iniciar Lavadora");
+            WasherController.startCycle();
+        }
+        else if (comando == '2') {
+            Serial.println("Comando recebido: Iniciar Secadora");
+            DryerController.startCycle();
+        }
+    }
 }
